d570af714e8a35be485a7df03f46e9fda0b4c791
eddis <mpp19@pitt.edu>
2014-03-28 13:08:46 -0400

    Update starterator.sh

--- starterator.sh
+++ starterator.sh
@@ -5,13 +5,7 @@ starterator_path=`dirname $abspath`
 echo $starterator_path
 
 cd $starterator_path
-<<<<<<< HEAD
+
 git pull
-<<<<<<< HEAD
-=======
-bzr pull
->>>>>>> 17a3d1a2438d87df8c80286abfafb97e14028e9f
 $starterator_path/Starterator
-=======
 python $starterator_path/Starterator
->>>>>>> refactor
